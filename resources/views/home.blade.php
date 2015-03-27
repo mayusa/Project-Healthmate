7a0f0f0b3f76870e4d840489ba4d4fb1ae7bbfd7
mayusa <ashucn@gmail.com>
2015-03-26 23:43:18 -0700

    adding reset password func

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading">Home Page</div>
 
 				<div class="panel-body">
                     @if (Session::has('errmsg'))
