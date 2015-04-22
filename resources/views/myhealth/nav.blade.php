4bfbce1c90fc143585b8765434f954200ec5289a
mayusa <ashucn@gmail.com>
2015-04-21 21:05:08 -0700

    modify nav

--- resources/views/myhealth/nav.blade.php
+++ resources/views/myhealth/nav.blade.php
@@ -8,22 +8,22 @@
 	  	</h4>
 	  	</a>
 	  </li>
-	  <li @if( strpos(Request::url(), '/myhealth/insurances') ) class="active" @endif >
+{{-- 	  <li @if( strpos(Request::url(), '/myhealth/insurances') ) class="active" @endif >
 	  	<a href="/myhealth/insurances/home">
 	  	<h4><span class="fa fa-life-ring"></span><span class="hidden-xs"> <small> My Insurance</small></span></h4>
 	  	
 	  	</a>
-	  </li>
+	  </li> --}}
 	  <li @if( strpos(Request::url(), '/myhealth/appointments') ) class="active" @endif >
 	  	<a href="/myhealth/appointments/home">
 	  	<h4><span class="fa fa-calendar"></span> <span class="hidden-xs"><small> My Appointments </small></span></h4>
 	  	</a>
 	  </li>
-	  <li @if( strpos(Request::url(), '/myhealth/conditions') ) class="active" @endif >
+{{-- 	  <li @if( strpos(Request::url(), '/myhealth/conditions') ) class="active" @endif >
 	  	<a href="/myhealth/conditions/home">
 	  	<h4><span class="fa fa-heartbeat"></span> <span class="hidden-xs"><small> My Conditions </small></span></h4>
 	  	
 	  	</a>
-	  </li>
+	  </li> --}}
 	</ul>
 </div>
